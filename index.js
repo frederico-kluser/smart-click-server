272e52371ad672c1456fae8042bb6f46371cb51d
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-04-09 13:21:40 -0300

    change cors function

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors());
 
 app.use(express.static('public')); // optional to se static files by current directory
 
