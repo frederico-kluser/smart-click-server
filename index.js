e616caa86a829d7270a1f3d2b1866e6d32ad1ab4
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-04-09 03:38:45 -0300

    fix CORS problems

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(express.static('public')); // optional to se static files by current directory
 
 app.use('/macro', require('./routes/macroRouter'));
